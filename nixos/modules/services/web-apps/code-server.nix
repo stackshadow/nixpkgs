4bd4f4717e5b6d2004e6dc1c5f6c252f242affbe
Martin <stackshadow@evilbrain.de>
2021-08-30 19:15:56 +0200

    code-server: Apply suggestions from code review
    - add `extra-environment` to specify extra env-vars
    - add default group to default-user
    - thanks to aanderse for the review and the hints

--- nixos/modules/services/web-apps/code-server.nix
+++ nixos/modules/services/web-apps/code-server.nix
@@ -1,69 +1,92 @@
 { config, lib, pkgs, ... }:
 
 with lib;
-let cfg = config.services.code-server;
+let
+
+  cfg = config.services.code-server;
+  defaultUser = "code-server";
+  defaultGroup = defaultUser;
 
 in {
   ###### interface
   options = {
     services.code-server = {
-      enable = mkEnableOption "enable code-server";
+      enable = mkEnableOption "code-server";
 
       package = mkOption {
         default = pkgs.code-server;
         defaultText = "pkgs.code-server";
-        description = "Which code-server derivation to use";
+        description = "Which code-server derivation to use.";
         type = types.package;
       };
 
       packages = mkOption {
         default = [ ];
         defaultText = "[]";
-        description = "Packages that are available in the PATH of code-server";
+        description = "Packages that are available in the PATH of code-server.";
         example = "[ pkgs.go ]";
         type = types.listOf types.package;
       };
 
+      extra-environment = mkOption {
+        type = types.attrsOf types.str;
+        description =
+          "Additional environment variables to passed to code-server.";
+        default = { };
+        example = { PKG_CONFIG_PATH = "/run/current-system/sw/lib/pkgconfig"; };
+      };
+
       host = mkOption {
         default = "127.0.0.1";
-        defaultText = "Locahost-IP";
-        description = "The host-ip to bind to";
+        description = "The host-ip to bind to.";
         type = types.str;
       };
 
       port = mkOption {
-        default = "4444";
-        defaultText = "4444";
-        description = "The port where code-server runs";
-        type = types.str;
+        default = 4444;
+        description = "The port where code-server runs.";
+        type = types.port;
       };
 
       auth = mkOption {
         default = "password";
-        defaultText = "Use password";
-        description = "The type of authentication to use. [password, none]";
-        type = types.str;
+        description = "The type of authentication to use.";
+        type = types.enum [ "none" "password" ];
       };
 
       hashedPassword = mkOption {
         default = "";
-        defaultText = "The hashed password, need auth set to 'password'";
-        description = "Create the password with: 'echo -n 'thisismypassword' | npx argon2-cli -e'";
+        description =
+          "Create the password with: 'echo -n 'thisismypassword' | npx argon2-cli -e'.";
         type = types.str;
       };
 
       user = mkOption {
-        default = "code-server";
-        defaultText = "The internal code-server user";
-        description = "Set the username for code-server. You can set this to your username to access your own files";
+        default = defaultUser;
+        example = "yourUser";
+        description = ''
+          The user to run Syncthing as.
+          By default, a user named <literal>${defaultUser}</literal> will be created.
+        '';
+        type = types.str;
+      };
+
+      group = mkOption {
+        default = defaultGroup;
+        example = "yourGroup";
+        description = ''
+          The group to run code-server under.
+          By default, a group named <literal>${defaultGroup}</literal> will be created.
+        '';
         type = types.str;
       };
 
       groups = mkOption {
-        default = "[]";
+        default = [ ];
         defaultText = "[]";
-        description = "An array of additional groups for the code-server user";
-        example = ''[ "docker" ]'';
+        description =
+          "An array of additional groups for the <literal>${defaultUser}</literal> user.";
+        example = [ "docker" ];
         type = types.listOf types.str;
       };
 
@@ -72,22 +95,22 @@ in {
 
   ###### implementation
   config = mkIf cfg.enable {
-    systemd.services.code-server = let
-
-    in {
+    systemd.services.code-server = {
       description = "VSCode server";
       wantedBy = [ "multi-user.target" ];
       after = [ "network-online.target" ];
       path = cfg.packages;
       environment = {
-        HASHED_PASSWORD = "${cfg.hashedPassword}";
-        PKG_CONFIG_PATH = "/run/current-system/sw/lib/pkgconfig";
-      };
+        HASHED_PASSWORD = cfg.hashedPassword;
+      } // cfg.extra-environment;
       serviceConfig = {
-        ExecStart = "${cfg.package}/bin/code-server --disable-telemetry --bind-addr ${cfg.host}:${cfg.port} --auth ${cfg.auth}";
+        ExecStart =
+          "${cfg.package}/bin/code-server --disable-telemetry --bind-addr ${cfg.host}:${
+            toString cfg.port
+          } --auth ${cfg.auth}";
         ExecReload = "${pkgs.coreutils}/bin/kill -HUP $MAINPID";
-        RuntimeDirectory = "${cfg.package}";
-        User = "${cfg.user}";
+        RuntimeDirectory = cfg.user;
+        User = cfg.user;
         Restart = "on-failure";
         # for ping probes
         AmbientCapabilities = [ "CAP_NET_RAW" ];
@@ -96,14 +119,15 @@ in {
 
     };
 
-    users.users.code-server =
-      mkIf (cfg.user == "code-server") {
-        isNormalUser = true;
-        createHome = true;
-        description = "code-server user";
-        home = "/home/code-server";
-        extraGroups = cfg.groups;
-        shell = pkgs.zsh;
-      };
+    users.users."${defaultUser}" = mkIf (cfg.user == defaultUser) {
+      isNormalUser = true;
+      group = cfg.group;
+      extraGroups = cfg.groups;
+      description = "code-server user";
+      shell = pkgs.bash;
+    };
+
+    users.groups."${defaultGroup}" = mkIf (cfg.group == defaultGroup) { };
+
   };
 }
